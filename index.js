47401adc316e337cf9e0424f2803e0abe9e6da0d
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-01-27 17:06:02 -0500

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(array) {
+   function win(element){
+        return element.result === "W"
+        }
+const winYes = array.find(win)
+if(winYes){
+    return winYes.year
+}
+return undefined
+}
+    
+
